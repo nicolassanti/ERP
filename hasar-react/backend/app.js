840dbec41eeb76f7d07658eb5be71f20c800c038
nicolas santi <nicolas.santi@outlook.com.ar>
2022-04-13 20:11:24 -0300

    ruteo de productos y usuarios terminado

--- hasar-react/backend/app.js
+++ hasar-react/backend/app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+ const express = require('express');
+ const path = require('path');
+ const cookieParser = require('cookie-parser');
+ const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+ const usersRouter = require('./routes/users.routes');
+ const productsRouter = require('./routes/products.routes');
 
-var app = express();
+ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+
+
 
 module.exports = app;
